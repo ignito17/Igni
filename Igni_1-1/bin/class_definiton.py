da729bcda5e7502767e9de7e1f895a9f87533dc7
Ignito <prabhat3055@gmail.com>
2024-11-01 22:27:45 +0530

    This is a commit from my personal laptop I am learning to use git that is why this confusion.

--- Igni_1-1/bin/class_definiton.py
+++ Igni_1-1/bin/class_definiton.py
@@ -1,11 +1,15 @@
 # Cointains class definitions.
 
 import path_definitons as p_d
+from data_process import import_modules
+dt=import_modules.datetime
 
 # igni is the blueprint for base custom object
 class igni():
     def __init__(self,name) -> None:
-        self.name=str(name)
+        self.obj_details={"object_name":str(name),
+                          "objec_type":"Igni",
+                          "creation_time":dt.now()}
         self.__obj_data_storage=p_d.path["data_store"]+"obj_name"
         pass
     def get_obj_data_storage_path(self):
@@ -13,3 +17,4 @@ class igni():
 
 to_do=igni("to_do")
 to_do.get_obj_data_storage_path()
+to_do.get_object_details()
---
+++ Igni_1-1/igni.py
@@ -0,0 +1,4 @@
+from bin import *
+from data_process import *
+from data_store import *
+
