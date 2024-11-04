f637767047a15a081256641bf82293f044e07c67
Ignito <prabhat3055@gmail.com>
2024-11-04 08:08:37 +0530

    Shutting down the use of personal laptop , added a property of  class topic.

--- Igni_1-1/bin/class_definitions.py
+++ Igni_1-1/bin/class_definitions.py
@@ -20,9 +20,24 @@ class topic(igni):
         self.__obj_name=name.strip()
         self.__obj_name=self.__obj_name.replace(" ","_")
         self.__obj_data_storage_path=ii.path_dict["data_store"]["topics"]+f"{self.__obj_name}"
+        self.__timestamp_dict={"Start":None,
+                          "Pause":None,
+                          "Stop":None}
     
     def _get_storage_path(self):
         return self.__obj_data_storage_path
+
+    # A fucntion to show dates
+    def _show_timestamps(self):
+        # f_string=f"Topic object {self.__obj_name}"
+        '''
+        for timestamp in self.__timestamp_dict.keys():
+            if self.__timestamp_dict[timestamp]:
+                yield [timestamp,self.__timestamp_dict[timestamp]]
+            else:
+                yield [timestamp,None]
+        '''
+        yield (timestamp, time) for timestamp, time in self.__timestamp_dict.items()
 
 # This is the blueprint for custom "tracker" object
 class tracker(igni):
--- Igni_1-1/igni.py
+++ Igni_1-1/igni.py
@@ -1,6 +1,12 @@
 from bin import class_definitions as c_d
 
+# topic object
 ds_obj=c_d.topic(name="Data Science")
 print(ds_obj._get_storage_path())
+for x in ds_obj._show_timestamps():
+    print(x, "\t", type(x))
+
+
+# tracker object
 ds_tracker_obj=c_d.tracker(name="ds_tracker")
 print(ds_tracker_obj._get_storage_path())
